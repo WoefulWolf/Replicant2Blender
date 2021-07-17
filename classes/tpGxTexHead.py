2ed67033cfa81906435c47c8c3189cfdc67c21f4
Woeful_Wolf <54476280+WoefulWolf@users.noreply.github.com>
2021-07-17 12:45:51 +0200

    Added tpGxTexHead class

---
+++ classes/tpGxTexHead.py
@@ -0,0 +1,48 @@
+from ..util import *
+
+class tpGxTexHead:
+    def __init__(self, packFile):
+        self.header = Header(packFile)
+
+        packFile.seek(self.header.offsetMipSurfaces)
+        self.mipSurfaces = []
+        for i in range(self.header.numMipSurfaces):
+            self.mipSurfaces.append(MipSurface(packFile))
+
+class Header:
+    def __init__(self, packFile):
+        self.width = to_uint(packFile.read(4))
+        self.height = to_uint(packFile.read(4))
+
+        self.numSurfaces = to_uint(packFile.read(4))
+
+        self.unknownUInt32_0 = to_uint(packFile.read(4))
+
+        self.filesize = to_uint(packFile.read(4))
+
+        self.unknownUInt32_1 = to_uint(packFile.read(4))
+
+        self.XonSurfaceFormat = XonSurfaceDXGIFormat(to_uint(packFile.read(4)))
+
+        self.numMipSurfaces = to_uint(packFile.read(4))
+        offsetToMipSurfaces = to_uint(packFile.read(4))
+        self.offsetMipSurfaces = packFile.tell() + offsetToMipSurfaces - 4
+
+
+class MipSurface:
+    def __init__(self, packFile):
+        self.offset = to_uint(packFile.read(4))
+
+        self.unknownUInt32_0 = to_uint(packFile.read(4))
+        self.unknownUInt32_1 = to_uint(packFile.read(4))
+        self.unknownUInt32_2 = to_uint(packFile.read(4))
+
+        self.size = to_uint(packFile.read(4))
+
+        self.unknownUInt32_3 = to_uint(packFile.read(4))
+
+        self.width = to_uint(packFile.read(4))
+        self.height = to_uint(packFile.read(4))
+
+        self.unknownUInt32_4 = to_uint(packFile.read(4))
+        self.unknownUInt32_5 = to_uint(packFile.read(4))
