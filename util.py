97fcdcad8e7c2b5958b8b9a5b4d8d5bed863bab7
Woeful_Wolf <54476280+WoefulWolf@users.noreply.github.com>
2021-07-17 12:46:42 +0200

    Added some conversion utils and DXGI formats

--- util.py
+++ util.py
@@ -4,6 +4,7 @@ import sys
 import struct
 import numpy as np
 import math
+from enum import Enum
 
 def to_float(bs):
 	return struct.unpack("<f", bs)[0]
@@ -26,3 +27,45 @@ def to_string(bs, encoding = 'utf8'):
 def alignRelative(openFile, relativeStart, alignment):
 	alignOffset = (((openFile.tell() - relativeStart) // alignment) + 1) * alignment
 	openFile.seek(relativeStart + alignOffset)
+
+def str_to_bytes(var):
+	return bytearray(var, 'utf-8')
+
+def uint32_to_bytes(var):
+	return var.to_bytes(4, byteorder='little', signed=False)
+
+def int32_to_bytes(var):
+	return var.to_bytes(4, byteorder='little', signed=True)
+
+class XonSurfaceDXGIFormat(Enum):
+    UNKNOWN = 0
+    R8G8B8A8_UNORM = 0x00010800
+    R8G8B8A8_UNORM_SRGB = 0x00010B00
+    BC1_UNORM = 0x00010F00
+    BC1_UNORM_SRGB = 0x00011000
+    BC2_UNORM = 0x00011100
+    BC2_UNORM_SRGB = 0x00011200
+    BC3_UNORM = 0x00011300
+    BC3_UNORM_SRGB = 0x00011400
+    BC4_UNORM = 0x00011500
+    BC5_UNORM = 0x00011600
+    R32G32B32A32_FLOAT = 0x00030000
+
+def get_DXGI_Format(surfaceFormat):
+	if (surfaceFormat == XonSurfaceDXGIFormat.UNKNOWN):
+		return "UNKNOWN"
+
+	if (surfaceFormat == XonSurfaceDXGIFormat.BC1_UNORM):
+		return 71
+
+	if (surfaceFormat == XonSurfaceDXGIFormat.BC1_UNORM_SRGB):
+		return 72
+
+	if (surfaceFormat == XonSurfaceDXGIFormat.BC3_UNORM):
+		return 77
+
+	if (surfaceFormat == XonSurfaceDXGIFormat.BC3_UNORM_SRGB):
+		return 78
+
+	if (surfaceFormat == XonSurfaceDXGIFormat.BC5_UNORM):
+		return 83
