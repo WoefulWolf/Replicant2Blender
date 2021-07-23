6070b48d7fe0beebe1530293b420fba4f8749315
Woeful_Wolf <54476280+WoefulWolf@users.noreply.github.com>
2021-07-23 12:26:24 +0200

    Added some DXGI formats
    (there are probably still some more unknown ones, just open an issue if you find them)

--- util.py
+++ util.py
@@ -38,18 +38,19 @@ def int32_to_bytes(var):
 	return var.to_bytes(4, byteorder='little', signed=True)
 
 class XonSurfaceDXGIFormat(Enum):
-    UNKNOWN = 0
-    R8G8B8A8_UNORM = 0x00010800
-    R8G8B8A8_UNORM_SRGB = 0x00010B00
-    BC1_UNORM = 0x00010F00
-    BC1_UNORM_SRGB = 0x00011000
-    BC2_UNORM = 0x00011100
-    BC2_UNORM_SRGB = 0x00011200
-    BC3_UNORM = 0x00011300
-    BC3_UNORM_SRGB = 0x00011400
-    BC4_UNORM = 0x00011500
-    BC5_UNORM = 0x00011600
-    R32G32B32A32_FLOAT = 0x00030000
+	UNKNOWN = 0
+	R8G8B8A8_UNORM = 0x00010800
+	R8G8B8A8_UNORM_SRGB = 0x00010B00
+	BC1_UNORM = 0x00010F00
+	BC1_UNORM_SRGB = 0x00011000
+	BC2_UNORM = 0x00011100
+	BC2_UNORM_SRGB = 0x00011200
+	BC3_UNORM = 0x00011300
+	BC3_UNORM_SRGB = 0x00011400
+	BC4_UNORM = 0x00011500
+	BC5_UNORM = 0x00011600
+	R32G32B32A32_FLOAT = 0x00030000
+	A8_UNORM = 0x00031700
 
 def get_DXGI_Format(surfaceFormat):
 	if (surfaceFormat == XonSurfaceDXGIFormat.UNKNOWN):
@@ -61,11 +62,26 @@ def get_DXGI_Format(surfaceFormat):
 	if (surfaceFormat == XonSurfaceDXGIFormat.BC1_UNORM_SRGB):
 		return 72
 
+	if (surfaceFormat == XonSurfaceDXGIFormat.BC2_UNORM):
+		return 74
+
+	if (surfaceFormat == XonSurfaceDXGIFormat.BC2_UNORM_SRGB):
+		return 75
+
 	if (surfaceFormat == XonSurfaceDXGIFormat.BC3_UNORM):
 		return 77
 
 	if (surfaceFormat == XonSurfaceDXGIFormat.BC3_UNORM_SRGB):
 		return 78
+	
+	if (surfaceFormat == XonSurfaceDXGIFormat.BC4_UNORM):
+		return 80
 
 	if (surfaceFormat == XonSurfaceDXGIFormat.BC5_UNORM):
 		return 83
+
+	if (surfaceFormat == XonSurfaceDXGIFormat.R32G32B32A32_FLOAT):
+		return 3
+
+	if (surfaceFormat == XonSurfaceDXGIFormat.A8_UNORM):
+		return 65
